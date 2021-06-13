f4e7fbe29dea7f13732b3294d24ee08b1948de57
201411108 <aggsae@gmail.com>
2021-06-14 00:17:52 +0900

    구현: 사용할 element 및 속성 정의

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+// Elements
+const clockDiv = document.querySelector('.clock-div')
+const title = document.querySelector('.title')
+const clock = document.querySelector('.clock')
+
+const nameDiv = document.querySelector('.name-div')
+const nameForm = document.querySelector('.name-form')
+const nameInput = document.querySelector('.name-input')
+
+const mainContent = document.querySelector('.main-content')
+const today = document.querySelector('.today')
+const todayTodoDiv = document.querySelector('.today-todo-div')
+const todoTodoUl = document.querySelector('.today-todo-ul');
+
+const weatherDiv = document.querySelector('.weather-div')
+
+const plan = document.querySelector('.plan')
+const upcomingDiv = document.querySelector('.upcoming-div')
+const timeMachineDiv = document.querySelector('.time-machine-div')
+
+// Properties
+const USER_NAME = 'user_name'
+const HIDE = 'hiding'
+const SHOW = 'showing'
+const FLEX = 'flex'
